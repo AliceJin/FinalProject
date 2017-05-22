1a9db8ae5ba10468ace400933aa1698a44a7d4e0
lie5571 <Yifanli2009@gmail.com>
2017-05-22 08:05:56 -0700

    update

--- src/sample/Ethan2.java
+++ src/sample/Ethan2.java
@@ -7,12 +7,22 @@ import javafx.stage.Stage;
 
 public class Ethan2 extends Application {
 
+    Button button;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Title of the Window");
+        button = new Button();
+        button.setText("Click me");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
