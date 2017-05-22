934ecd573af6a655dfaa2cf24f165113bdfdbf73
lie5571 <Yifanli2009@gmail.com>
2017-05-22 09:16:14 -0700

    update

--- src/sample/Ethan2.java
+++ src/sample/Ethan2.java
@@ -1,8 +1,10 @@
 package sample;/**
  * Created by jinq7372 on 5/18/2017.
  */
-
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Ethan2 extends Application {
@@ -14,8 +16,8 @@ public class Ethan2 extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Title of the Window");
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("I hate my life");
         button = new Button();
         button.setText("Click me");
         StackPane layout = new StackPane();
@@ -23,6 +25,6 @@ public class Ethan2 extends Application {
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
+
 }
