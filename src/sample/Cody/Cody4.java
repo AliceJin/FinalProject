c5e3eb190418d20542f52920dc057a7c2caa8700
StopwatchCAMEL <Cody.lin@live.com>
2017-05-24 10:25:01 -0700

    CODY CHANGES

---
+++ src/sample/Cody/Cody4.java
@@ -0,0 +1,28 @@
+package sample;/**
+ * Created by jinq7372 on 5/18/2017.
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Cody4 extends Application  {
+
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+}
