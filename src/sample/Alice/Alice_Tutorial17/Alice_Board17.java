940bc3215854dc1e38470db5191a4699ed9d62b7
AliceJin <snowjay99@gmail.com>
2017-05-31 09:17:59 -0700

    Added copy of Battleship files

--- src/sample/Alice/Alice_Tutorial17/Alice_Board17.java
+++ src/sample/Alice/Alice_Tutorial17/Alice_Board17.java
@@ -1,4 +1,4 @@
-package sample.Alice.Alice_Tutorial17;/**
+package sample.Alice.Alice_Tutorial17; /**
  * Created by jinq7372 on 5/26/2017.
  */
 
@@ -22,7 +22,7 @@ import javafx.scene.shape.Rectangle;      //super class for cells
 
 /**
  * extends Parent class, allow to show on game board
- * NOTE TO GROUP MEMBERS: you have to change Alice_Board17 and others to
+ * NOTE TO GROUP MEMBERS: you have to change sample.Alice.Alice_Tutorial17.Alice_Board17 and others to
  * the appropriate class name for your file.
  */
 public class Alice_Board17 extends Parent {
@@ -35,15 +35,12 @@ public class Alice_Board17 extends Parent {
      * Takes parameters: whether board is the enemy's (boolean) and
      * and handler for processing mouse clicks on cells
      */
-    public Alice_Board17(boolean enemy, EventHandler<? super MouseEvent> handler)
-    {
+    public Alice_Board17(boolean enemy, EventHandler<? super MouseEvent> handler) {
         this.enemy = enemy;               //set variable equal to input
         //loop for establishing number of boxes, 10 by 10
-        for(int y = 0; y < 10; y++)
-        {
+        for (int y = 0; y < 10; y++) {
             HBox row = new HBox();        //place boxes horizontally
-            for(int x = 0; x < 10; x++)
-            {
+            for (int x = 0; x < 10; x++) {
                 Cell c = new Cell(x, y, this);     //this refers to board
                 c.setOnMouseClicked(handler);             //trigger on mouse click
                 row.getChildren().add(c);                 //add cells as children to rows
@@ -58,35 +55,28 @@ public class Alice_Board17 extends Parent {
      * Parameters: ship, x and y coordinates as integers.
      * Returns: boolean to show whether ship was placed.
      */
-    public boolean placeShip(Alice_Ship17 ship, int x, int y)
-    {
+    public boolean placeShip(Alice_Ship17 ship, int x, int y) {
         //if statement: tests whether one can place the ship, returns false otherwise
-        if(canPlaceShip(ship, x, y))
-        {
+        if (canPlaceShip(ship, x, y)) {
             int length = ship.type;     //store length in local variable
 
-            if(ship.vertical)
-            {
+            if (ship.vertical) {
                 //if ship is vertical, color ship vertically along the length from y coordinate
-                for(int i = y; i < y + length; i++)
-                {
+                for (int i = y; i < y + length; i++) {
                     Cell cell = getCell(x, i);      //current cell in loop
                     cell.ship = ship;               //ship in cell is referenced to current ship
-                    if(!enemy)                      //if not the enemy board
+                    if (!enemy)                      //if not the enemy board
                     {
                         cell.setFill(Color.WHITE);     //fill in white
                         cell.setStroke(Color.GREEN);   //green border
                     }
                 }
-            }
-            else
-            {
+            } else {
                 //if ship is horizontal. color ship horizontally along length from x coordinate
-                for(int i = x; i < x + length; i++)
-                {
+                for (int i = x; i < x + length; i++) {
                     Cell cell = getCell(i, y);      //current cell in loop
                     cell.ship = ship;               //ship in cell is referenced to current ship
-                    if(!enemy)                      //if not the enemy board
+                    if (!enemy)                      //if not the enemy board
                     {
                         cell.setFill(Color.WHITE);     //fill in white
                         cell.setStroke(Color.GREEN);   //green border
@@ -103,10 +93,36 @@ public class Alice_Board17 extends Parent {
     /**
      * Method function: return the cell at x and y coordinates
      */
-    public Cell getCell(int x, int y)
-    {
+    public Cell getCell(int x, int y) {
         //returns the cell; get row first as a whole, set as HBox, and then get cell
-        return (Cell) ((HBox)rows.getChildren().get(y)).getChildren().get(x);
+        return (Cell) ((HBox) rows.getChildren().get(y)).getChildren().get(x);
+    }
+
+    /**
+     * Method function: return an array of neighboring cells that are within the board.
+     * @param x
+     * @param y
+     * @return array of neighboring cells
+     */
+    private Cell[] getNeighbors(int x, int y) {
+        Point2D[] points = new Point2D[]{     //array of points around the selected cell
+                new Point2D(x - 1, y),   //left
+                new Point2D(x + 1, y),   //right
+                new Point2D(x, y - 1),   //top
+                new Point2D(x, y + 1)    //bottom
+        };
+
+        //list of neighbors
+        List<Cell> neighbors = new ArrayList<Cell>();
+
+        for (Point2D p : points) {
+            if (isValidPoint(p)) {
+                //add cell to list if valid point
+                neighbors.add(getCell((int) p.getX(), (int) p.getY()));
+            }
+        }
+
+        return neighbors.toArray(new Cell[0]);
     }
 
     /**
@@ -115,37 +131,47 @@ public class Alice_Board17 extends Parent {
      * Parameters: ship, x and y coordinates as integers.
      * Returns: boolean to show whether ship can be placed.
      */
-    private boolean canPlaceShip(Alice_Ship17 ship, int x, int y)
-    {
+    private boolean canPlaceShip(Alice_Ship17 ship, int x, int y) {
         int length = ship.type;       //store length in local variable
 
-        if(ship.vertical)             //if ship is meant to be vertical
+        if (ship.vertical)             //if ship is meant to be vertical
         {
-            for(int i = y; i < y + length; i++)
-            {
+            for (int i = y; i < y + length; i++) {
                 if (!isValidPoint(x, i))
                     return false;     //if cell is not in grid, return false
 
                 Cell cell = getCell(x, i);     //if valid, get the cell
-                if(cell.ship != null)
+                if (cell.ship != null)
                     return false;              //if there is already a ship, return false
 
-                /**
-                 * NOTE by Alice: I'm not going to include the neighbors mechanism
-                 * that doesn't allow the user to place ships next to each other.
-                 */
+                //neighbors method - cannot place ship neighboring to each other
+                //left, right, top, and bottom
+                for (Cell neighbor : getNeighbors(x, i))
+                {
+                    if (!isValidPoint(x, i))
+                        return false;                  //if neighbors valid points, return false
+
+                    if (neighbor.ship != null)
+                        return false;                  //if neighbors contain ships, return false
+                }
             }
         }
-        else
-        {
-            for(int i = x; i < x + length; i++)
-            {
+        else {           //horizontal ships
+            for (int i = x; i < x + length; i++) {
                 if (!isValidPoint(i, y))
                     return false;     //if cell is not in grid, return false
 
                 Cell cell = getCell(i, y);     //if valid, get the cell
-                if(cell.ship != null)
+                if (cell.ship != null)
                     return false;              //if there is already a ship, return false
+
+                for (Cell neighbor : getNeighbors(i, y)) {
+                    if (!isValidPoint(i, y))
+                        return false;               //if neighbors valid points, return false
+
+                    if (neighbor.ship != null)
+                        return false;               //if neighbors contain ships, return false
+                }
             }
         }
         return true;
@@ -156,8 +182,7 @@ public class Alice_Board17 extends Parent {
      * format as a point instead of coordinates as doubles.
      * Passes input to the overridden same method.
      */
-    private boolean isValidPoint(Point2D point)
-    {
+    private boolean isValidPoint(Point2D point) {
         return isValidPoint(point.getX(), point.getY());  //get the x and y coordinates
     }
 
@@ -166,8 +191,7 @@ public class Alice_Board17 extends Parent {
      * Parameters: x and y coordinates as doubles.
      * Returns: boolean for whether point is valid or not.
      */
-    private boolean isValidPoint(double x, double y)
-    {
+    private boolean isValidPoint(double x, double y) {
         //coordinates greater than/equal to zero and less than ten
         return x >= 0 && x < 10 && y >= 0 && y < 10;
     }
@@ -186,8 +210,7 @@ public class Alice_Board17 extends Parent {
          * Constructor with parameters
          * x and y coordinates (int) and the board (Board)
          */
-        public Cell(int x, int y, Alice_Board17 board)
-        {
+        public Cell(int x, int y, Alice_Board17 board) {
             super(30, 30);                //calls Rectangle class with 30 by 30 pixels
             this.x = x;                   //sets variables equal to input
             this.y = y;
@@ -200,13 +223,12 @@ public class Alice_Board17 extends Parent {
          * Method function: changes color appropriately when hit by enemy.
          * Sets different colors depending on whether cell contained a ship or not
          */
-        public boolean shoot()
-        {
-            wasShot = true;               //ship is shot
+        public boolean shoot() {
+            wasShot = true;               //cell is shot
             setFill(Color.BLACK);         //set to black if cell doesn't have ship
 
             //if cell contained ship, set to red
-            if(ship != null) {
+            if (ship != null) {
                 ship.hit();               //decrease health, method in Ship class
                 setFill(Color.RED);       //set color to red, override
                 //if ship is dead
